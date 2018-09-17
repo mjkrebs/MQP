a1f64e1f72d7dc752f3b18404c78113153e185be
Jake Scheide <jcscheide@wpi.edu>
2018-09-16 20:51:26 -0400

    it gets the draft position now mostly, except a few outliers

--- expectedDraftPosition.py
+++ expectedDraftPosition.py
@@ -19,17 +19,17 @@ def getPercentileMetricCareer(name):
 
 #creates a set of all players.
 def addPlayers():
-    players = set()
-    x = 1
-    for year in range(1990,2019):
-        master = xlrd.open_workbook("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
-        m_sheet = master.sheet_by_index(0)
-        for i in range(1,m_sheet.nrows):
-            result = m_sheet.cell(i,0).value
-            if(result not in players):
-                players.add(result)       
-                x = x+1
-    return players
+	players = set()
+	x = 1
+	for year in range(1990,2019):
+		master = xlrd.open_workbook("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
+		m_sheet = master.sheet_by_index(0)
+		for i in range(1,m_sheet.nrows):
+			result = m_sheet.cell(i,0).value
+			if(result not in players):
+				players.add(result)       
+				x = x+1
+	return players
 
 
 #create a workbook with 61 worksheets, one for each draft pick AND one for undrafted.
@@ -41,9 +41,19 @@ def createWorkbook():
 		worksheet.write(0,1,"metric=percentile")
 	return workbook
 
-#look up name in draft workbook. Currently returns a random number between 1 and 61, inclusive. 61 == undrafted.
+#look up name in draft workbook. 61 == undrafted.
 def getPlayerDraftPosn(playerName):
-	return randint(1,61)
+	for year in range(1990,2019):
+		master = xlrd.open_workbook("Resources/" + str(year) + "/Draft_" + str(year) + ".xlsx")
+		m_sheet = master.sheet_by_index(0)
+		for i in range(1,m_sheet.nrows):
+			result = m_sheet.cell(i,2).value
+			if(result == playerName):
+				draftPos = m_sheet.cell(i,0).value
+				return int(draftPos)
+	return 61
+
+
 
 
 #start code
@@ -52,7 +62,7 @@ def getPlayerDraftPosn(playerName):
 metric = input("enter metric: options = percentile")
 
 playerSet = addPlayers()
-workbook = createWorkbook()
+playersByDraftPosition = createWorkbook()
 #currentRow tracks the correct row to insert the next player for each pick. just an array of ones initially.
 currentRow = np.ones(61, dtype=int)
 
@@ -62,15 +72,14 @@ for playerName in playerSet:
 	#testing print statement
 	print("Name: " + playerName + " draftPos: " + str(draftPos) + " percentileValue: " + str(percentileValue))
 	#open the excel file to the right worksheet
-	worksheet = workbook.get_worksheet_by_name("Pick " + str(draftPos))
+	worksheet = playersByDraftPosition.get_worksheet_by_name("Pick " + str(draftPos))
 	#add name and metric. need to find a way to better abstract this, perhaps create an array of all metrics
 	#then write an addRow function.
 	worksheet.write(currentRow[draftPos-1],0,playerName)
 	worksheet.write(currentRow[draftPos-1],1, percentileValue)
 	#increment the count. zero-indexed array so the first pick is the zero'th array, etc. 
 	currentRow[draftPos-1] = currentRow[draftPos-1] + 1
-	
-workbook.close()
+playersByDraftPosition.close()
 
 
 
