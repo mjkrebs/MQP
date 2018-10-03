110b61316f53ea32e99a8c2c7663c637bcb7e22c
Michael Krebs <mkrebs326@gmail.com>
2018-10-03 17:10:37 -0400

    made binary search for a player in master player

--- createPlayerFile.py
+++ createPlayerFile.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import time
 
 
 def all_players():
@@ -13,8 +14,32 @@ def all_players():
                 all_names.append((player[1]))
     return all_pid,all_names
 
-def find_player(pid):
-    df = pd.read_excel("Master_Players.xlsx")
-    df = df.loc[df['PID'] == pid]
-    return df.values
+
+def binary_search(arr, l, r, x):
+    # Check base case
+    if r >= l:
+        mid = int(l + (r - l) / 2)
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return arr[mid]
+            # If element is smaller than mid, then it
+        # can only be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, l, mid - 1, x)
+            # Else the element can only be present
+        # in right subarray
+        else:
+            return binary_search(arr, mid + 1, r, x)
+    else:
+        return -1
+
+def find_player(pid, binary):
+    if binary == 1:
+        pids = pd.read_excel("Master_Players.xlsx")["PID"].values
+        return binary_search(pids, 0, len(pid)-1, pid)
+    else:
+        df = pd.read_excel("Master_Players.xlsx")
+        df = df.loc[df['PID'] == pid]
+        return df.values
+
 
