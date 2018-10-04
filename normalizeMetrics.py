a5177bb73d8108c2b6968861bddb6003497fbb48
Jake Scheide <jcscheide@wpi.edu>
2018-10-04 14:01:02 -0400

    updates, might have nan fixed

--- normalizeMetrics.py
+++ normalizeMetrics.py
@@ -8,49 +8,57 @@
 
 
 import pandas as pd 
-
+import math
 #returns a metric value as a proportion of the maximum value, max = 100
 def normalizeFunction(value,maxValue):
 	return (value / maxValue) * 100
 
 #finds the maximum value of the given metric, then normalizes all values and saves in the master season sheets.
 def	normalizeMetric(metric):
-	maxVal = 0
+	maxVal = -100
 	for year in range(1990,2019):
-		df = pd.read_excel("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
+		df = master_season_dfs[year-1990]
 		for row in df.itertuples():
 			metricValue = getattr(row,metric)
 			if(metricValue > maxVal):
 				maxVal = metricValue
-
+	print('Max Value: ' + str(maxVal))
 	for year in range(1990,2019):
-		df = pd.read_excel("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
-		df['normalized_' + metric] = df.apply(lambda row: normalizeFunction(row['PER'], maxVal), axis=1)
+		df = master_season_dfs[year-1990]
+		df['normalized_' + metric] = df.apply(lambda row: normalizeFunction(row[metric], maxVal), axis=1)
 		df.to_excel("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
 
 
 
-#normalizeMetric("PER")
 
-#this dataframe not up to date, has too many undrafted players. using for testing purposes, change later. 
-masterFrame = pd.read_excel("Master_Players.xlsx")
+
 
 
 #given a playerID and rookie year, return a pair of values: one the cumulative value of the metric over the career, the other the number of seasons played.
 def getPlayerMetricCareer(playerID, rookieYear,metric):
 	cumulativeValue = 0
-	numSeasons = 0
+	numSeasons = 1
+	stoppedFinding = 0
 	if(rookieYear <= 1989):
 		rookieYear = 1990
 	for year in range(rookieYear,2019):
-		df = pd.read_excel("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx")
+		df = master_season_dfs[year - 1990]
 		df = df.loc[df['PID'] == playerID]
 		for row in df.itertuples():
 			seasonValue = getattr(row,metric)
+			if(math.isnan(seasonValue)):
+				continue
 			numSeasons += 1
+			if(stoppedFinding > 1):
+				print("Comeback by: " + getattr(row, 'PID'))
+			stoppedFinding = 0
 			cumulativeValue += seasonValue
 			break
-	print (cumulativeValue, numSeasons)
+		stoppedFinding += 1
+		if(stoppedFinding == 3):
+			break
+	if(numSeasons > 1):
+		numSeasons -= 1
 	return cumulativeValue, numSeasons
 
 
@@ -59,11 +67,12 @@ def getPlayerMetricCareer(playerID, rookieYear,metric):
 
 def getAverageMetricForDraftPosition(df, draftPosition, metric):
 	df = df.loc[df['Pk'] == draftPosition]
+	numPlayersAtPick = 0
 	OGsum = 0
 	numPlayers = 0
 	totalSeasonsForPick = 0
 	for row in df.itertuples(index=True, name='Pandas'):
-
+		numPlayersAtPick += 1
 		numPlayers += 1
 		playerID = getattr(row,'PID')
 		print(playerID)
@@ -73,13 +82,50 @@ def getAverageMetricForDraftPosition(df, draftPosition, metric):
 		totalSeasonsForPick += playerSeasons
 
 
-	return OGsum / totalSeasonsForPick
+	return OGsum / totalSeasonsForPick, numPlayersAtPick
+
+
+
+master_season_dfs = list()
+for year in range (1990,2019):
+	master_season_dfs.append(pd.read_excel("Resources/" + str(year) + "/Master_" + str(year) + ".xlsx"))
+"""
+normalizeMetric("PER")
+normalizeMetric("WS")
+normalizeMetric("VORP")
+
+"""
+masterFrame = pd.read_excel("Master_Players.xlsx")
 
 normalized_PER_list = list()
+normalized_WS_list = list()
+normalized_VORP_list = list()
+numPlayersAtPick_list = list()
 
 for x in range (1,61):
-	value = getAverageMetricForDraftPosition(masterFrame,x,"normalized_PER")
-	normalized_PER_list.append(value)
-	print("Computed normalized_PER for draftPosition: " + str(x) + str(value))
+	PER_value, numPlayersAtPick = getAverageMetricForDraftPosition(masterFrame,x,"normalized_PER")
+	VORP_value, numPlayersAtPick = getAverageMetricForDraftPosition(masterFrame,x,"normalized_VORP")
+	WS_value, numPlayersAtPick = getAverageMetricForDraftPosition(masterFrame,x,"normalized_WS")
 
-pd.DataFrame(normalized_PER_list).to_excel("Normalized_PER_by_DraftPos")
+	normalized_PER_list.append(PER_value)
+	normalized_WS_list.append(WS_value)
+	normalized_VORP_list.append(VORP_value)
+	numPlayersAtPick_list.append(numPlayersAtPick)
+
+	print("Computed normalized_PER for draftPosition " + str(x) + ": " + str(PER_value))
+	print("Computed normalized_WS for draftPosition " + str(x) + ": " + str(WS_value))
+	print("Computed normalized_VORP for draftPosition " + str(x) + ": " + str(VORP_value))
+
+draftPositions = list()
+for x in range(1,61):
+	draftPositions.append(x)
+
+stats = [('Draft Position', draftPositions),
+		('Number of players computed for', numPlayersAtPick_list),
+		('Normalized PER', normalized_PER_list), 
+		('Normalized WS', normalized_WS_list),
+		 ('Normalized VORP', normalized_VORP_list)]
+stats_df = pd.DataFrame.from_items(stats)
+writer = pd.ExcelWriter('Normalized_Stats_by_DraftPos.xlsx')
+stats_df.to_excel(writer, 'Sheet1')
+writer.save()
